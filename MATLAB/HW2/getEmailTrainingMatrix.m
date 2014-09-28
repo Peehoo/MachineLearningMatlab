76a427156cde9d33a509d37a058dc237b8696124
Peehoo <dewan.peehoo@gmail.com>
2014-09-28 16:01:37 -0700

    Fixed Feature Vector

--- MATLAB/HW2/getEmailTrainingMatrix.m
+++ MATLAB/HW2/getEmailTrainingMatrix.m
@@ -5,14 +5,17 @@ spamTrainingDirectory = strcat(trainingDirectory, '/spam');
 listing = dir(spamTrainingDirectory);
 X_train = zeros(1, size(vocabList,1));
 Y_train = zeros(1, 1);
+row=1;
 for i=1:size(listing,1)
     if((strcmp(listing(i).name, '.'))==0)
         if((strcmp(listing(i).name, '..'))==0)
-            nameOfFile = strcat(spamTrainingDirectory, '/' , listing(i).name); 
+            row
+            nameOfFile = strcat(spamTrainingDirectory, '/' , listing(i).name);
             email_contents = readFile(nameOfFile);
             features = getFeatureVector(email_contents, vocabList);
-            X_train(i, :) = features;
-            Y_train(i, :) = 1;
+            X_train(row, :) = features;
+            Y_train(row, :) = 1;
+            row = row +1;
         end
     end
 end
@@ -23,13 +26,15 @@ hamTrainingDirectory = strcat(trainingDirectory, '/ham');
 
 listing = dir(hamTrainingDirectory);
 for j=1:size(listing,1)
-    if((strcmp(listing(i).name, '.'))==0)
-        if((strcmp(listing(i).name, '..'))==0)
-            nameOfFile = strcat(hamTrainingDirectory, '/' , listing(i).name); 
+    if((strcmp(listing(j).name, '.'))==0)
+        if((strcmp(listing(j).name, '..'))==0)
+            row
+            nameOfFile = strcat(hamTrainingDirectory, '/' , listing(j).name); 
             email_contents = readFile(nameOfFile);
             features = getFeatureVector(email_contents, vocabList);
-            X_train(i + j, :) = features;
-            Y_train(i + j, :) = 0;
+            X_train(row, :) = features;
+            Y_train(row, :) = 0;
+            row = row +1;
         end
     end
 end
--- MATLAB/HW2/getFeatureVector.m
+++ MATLAB/HW2/getFeatureVector.m
@@ -3,53 +3,66 @@ function features = getFeatureVector(email_contents, vocabList)
 % Process file
 l = 0;
 features = zeros(size(vocabList));
-while ~isempty(email_contents)
+C = strsplit(email_contents, {' ', '.', ',', '?', '\n', '\f', '\r', '\t'});
 
-    % Tokenize and also get rid of any punctuation
-    [str, email_contents] = strtok(email_contents, [' .?,\f\n\r\t\v' char(13)]);
-    
-    % Skip the word if it is too short
-    if length(str) < 1
-       continue;
-    end
-
-    % Look up the word in the dictionary and add to word_indices if
-    % found
-    % ====================== YOUR CODE HERE ======================
-    % Instructions: Fill in this function to add the index of str to
-    %               word_indices if it is in the vocabulary. At this point
-    %               of the code, you have a stemmed word from the email in
-    %               the variable str. You should look up str in the
-    %               vocabulary list (vocabList). If a match exists, you
-    %               should add the index of the word to the word_indices
-    %               vector. Concretely, if str = 'action', then you should
-    %               look up the vocabulary list to find where in vocabList
-    %               'action' appears. For example, if vocabList{18} =
-    %               'action', then, you should add 18 to the word_indices 
-    %               vector (e.g., word_indices = [word_indices ; 18]; ).
-    % 
-    % Note: vocabList{idx} returns a the word with index idx in the
-    %       vocabulary list.
-    % 
-    % Note: You can use strcmp(str1, str2) to compare two strings (str1 and
-    %       str2). It will return 1 only if the two strings are equivalent.
-    %
-
-
+length = size(C, 2);
+for i = 1:length
+    str = C(i);
     
     for i = 1:size (vocabList)
         if(strcmp(vocabList{i}, str) == 1)
             features(i) = features(i) +1;
         end
     end
-
-    % =============================================================
-
-
-    % Print to screen, ensuring that the output lines are not too long
-    if (l + length(str) + 1) > 78
-        l = 0;
-    end
-    l = l + length(str) + 1;
-
+    
 end
+% while ~isempty(email_contents)
+% 
+%     % Tokenize and also get rid of any punctuation
+%     [str, email_contents] = strtok(email_contents, [' .?,\f\n\r\t\v' char(13)]);
+%     
+%     % Skip the word if it is too short
+%     if length(str) < 1
+%        continue;
+%     end
+% 
+%     % Look up the word in the dictionary and add to word_indices if
+%     % found
+%     % ====================== YOUR CODE HERE ======================
+%     % Instructions: Fill in this function to add the index of str to
+%     %               word_indices if it is in the vocabulary. At this point
+%     %               of the code, you have a stemmed word from the email in
+%     %               the variable str. You should look up str in the
+%     %               vocabulary list (vocabList). If a match exists, you
+%     %               should add the index of the word to the word_indices
+%     %               vector. Concretely, if str = 'action', then you should
+%     %               look up the vocabulary list to find where in vocabList
+%     %               'action' appears. For example, if vocabList{18} =
+%     %               'action', then, you should add 18 to the word_indices 
+%     %               vector (e.g., word_indices = [word_indices ; 18]; ).
+%     % 
+%     % Note: vocabList{idx} returns a the word with index idx in the
+%     %       vocabulary list.
+%     % 
+%     % Note: You can use strcmp(str1, str2) to compare two strings (str1 and
+%     %       str2). It will return 1 only if the two strings are equivalent.
+%     %
+% 
+% 
+%     
+%     for i = 1:size (vocabList)
+%         if(strcmp(vocabList{i}, str) == 1)
+%             features(i) = features(i) +1;
+%         end
+%     end
+% 
+%     % =============================================================
+% 
+% 
+%     % Print to screen, ensuring that the output lines are not too long
+%     if (l + length(str) + 1) > 78
+%         l = 0;
+%     end
+%     l = l + length(str) + 1;
+% 
+% end
