574a6f76fd613949c9b161d1a2ffec85fb5956be
Peehoo <dewan.peehoo@gmail.com>
2014-10-08 00:49:19 -0700

    NM

--- MATLAB/HW2/NewtonMethod.m
+++ MATLAB/HW2/NewtonMethod.m
@@ -2,7 +2,7 @@ function [costMatrix, cost, theta_b, cost_test] = NewtonMethod(numIterations, X_
 
 lambda = 0.05;
 eta = 0.01;
-[ costMatrix, cost, theta, b ] = gradientDescent(lambda, eta, X_train, Y_train, 5)
+[ costm, cost, theta, b ] = gradientDescent(lambda, eta, X_train, Y_train, 5);
  
 % theta_b = [b;theta] ;  
 % 
@@ -28,12 +28,12 @@ identity(1,1) = 0;
 
 X_train = [ones(size(X_train,1),1) X_train];
 X_test = [ones(size(X_test,1),1) X_test];
-
+costMatrix = zeros(numIterations, 1);
 for i = 1 : numIterations
     Sig = sigmoid(X_train*theta_b);
 
     H = X_train'*diag(Sig)*diag(1-Sig)*X_train + lambda_1.*identity;
-    theta_b = theta_b - ((pinv(H)))*(X_train'*(Sig-Y_train) + 2*lambda_1*[0;theta_b(2:end)]);
+    theta_b = theta_b - ((pinv(H)))*(X_train'*(Sig-Y_train) + lambda_1*[0;theta_b(2:end)]);
         
     Sig = sigmoid(X_train*theta_b);
     %OneMinusSig = 1 - Sig;
@@ -41,7 +41,7 @@ for i = 1 : numIterations
     Sig(Sig>1-1e-16) = 1-1e-16;
     Sig(Sig<1e-16) = 1e-16;
     
-    cost = - Y_train'*log(Sig) - (1 - Y_train)'*(log(1-Sig)) + lambda_1*norm(theta_b(2:end),2)^2;
+    cost = - Y_train'*log(Sig) - (1 - Y_train)'*(log(1-Sig)) + lambda_1*norm(theta_b(2:end),2)^2
     costMatrix(i) = cost;
 end
 
